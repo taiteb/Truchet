2ed4d9289d874d3d90f1ab65a8dc4e436245b142
taiteb <taiteb@gmail.com>
2023-12-17 23:06:57 -0700

    fixed and improved drawing

--- sketch.js
+++ sketch.js
@@ -1,25 +1,28 @@
 function setup() {
   createCanvas(700, 700);
-  number_of_tiles = 10;
+  let c1 = color(145, 145, 30)
+  let c2 = color(30, 165, 80)
+  number_of_tiles = 30;
   size = width / number_of_tiles;
   background(255)
   strokeWeight(2);
-  for (x = 0; x < number_of_tiles + 1; x++) {
-    for (y = 0; y < number_of_tiles + 1; y++) {
+
+  for (x = 1; x < number_of_tiles - 1; x++) {
+    for (y = 1; y < number_of_tiles - 1; y++) {
       push();
       // translate(i * size, j * size);
-      let xpos = x * 70
-      let ypos = y * 70
-      // if (random(0, 2) < .3){
-      //   draw_lines(xpos, ypos);
-      // } else {
-      //   draw_curves(xpos, ypos);
-      // }
-      // stroke(155, 155, 155)
-      rect(xpos, ypos, size, size)
-      draw_circs(xpos, ypos)
-      // stroke(0)
-      // draw_lines(xpos, ypos)
+      let xpos = x * size
+      let ypos = y * size
+      strokeWeight(4)
+      stroke(c2)
+      which_to_draw = random(0, 3)
+      if (which_to_draw < 1) {
+        draw_circs(xpos, ypos);
+      } else if (which_to_draw < 2.7) {
+        draw_curves(xpos, ypos);
+      } else {
+        draw_lines(xpos, ypos)
+      }
       pop();
     }
   }
@@ -29,29 +32,16 @@ function draw() {
 }
 
 function draw_curves(xpos, ypos) {
-  // translate(xpos + (size / 2), ypos + (size / 2))
-  // rotate(floor(random(4)) * PI * 0.5)
-  // translate(-xpos, -ypos)
-  // fill(0);
-  // strokeWeight(4)
-  arc1 = arc(xpos, ypos, size, size, PI * 0.5, PI)
-  arc2 = arc(xpos, ypos, size, size, -PI * 0.5, 0)
-  return arc1
+  arc1 = arc(xpos, ypos + size, size, size, -HALF_PI, 0)
+  arc2 = arc(xpos + size, ypos, size, size, HALF_PI, PI)
 }
 
 function draw_circs(xpos, ypos) {
-  translate(xpos + (size / 2), ypos + (size / 2))
-  rotate(floor(random(2)) * PI * 0.5)
-  translate(-xpos, -ypos)
-  // fill(0);
-  // strokeWeight(4)
-  arc1 = arc(xpos, ypos, size, size, PI * 0.5, PI)
-  arc2 = arc(xpos, ypos, size, size, -PI * 0.5, 0)
-  return arc1
+  arc1 = arc(xpos, ypos, size, size, 0, HALF_PI)
+  arc2 = arc(xpos + size, ypos + size, size, size, PI, -HALF_PI)
 }
 
 function draw_lines(xpos, ypos) {
-  line1 = line(xpos + (size /2), ypos, xpos + (size /2), ypos + size);
-  line2 = line(xpos, ypos + (size /2), xpos + size, ypos + (size / 2));
-  return line1, line2
+  line1 = line(xpos + (size / 2), ypos, xpos + (size / 2), ypos + size);
+  line2 = line(xpos, ypos + (size / 2), xpos + size, ypos + (size / 2));
 }
