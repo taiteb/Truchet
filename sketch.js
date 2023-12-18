e94c193f0ae2a254e1c1cb6f382bff6a9588a74b
taiteb <taiteb@gmail.com>
2023-12-17 21:33:16 -0700

    changed coord handling

--- sketch.js
+++ sketch.js
@@ -1,27 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
-  number_of_tiles = 13;
+  createCanvas(700, 700);
+  number_of_tiles = 10;
   size = width / number_of_tiles;
   background(255)
   strokeWeight(2);
-  for (i = 0; i < number_of_tiles + 1; i++) {
-    for (j = 0; j < number_of_tiles + 1; j ++) {
+  for (x = 0; x < number_of_tiles + 1; x++) {
+    for (y = 0; y < number_of_tiles + 1; y++) {
       push();
-      translate(i * size, j * size);
-      centerPoint = new p5.Vector(size/2, size/2);
-      rotate(floor(random(4)) * PI * 0.5, centerPoint);
-      if (random(0, 2) < .3){
-        lines = draw_lines();
-        
-      } else {
-        draw_curves();
-      }
-      // draw_lines();
-
-      stroke(220, 0, 0);
-      strokeWeight(5);
-      noFill()
-      // square(0,0,size);
+      // translate(i * size, j * size);
+      let xpos = x * 70
+      let ypos = y * 70
+      // if (random(0, 2) < .3){
+      //   draw_lines(xpos, ypos);
+      // } else {
+      //   draw_curves(xpos, ypos);
+      // }
+      // stroke(155, 155, 155)
+      rect(xpos, ypos, size, size)
+      draw_circs(xpos, ypos)
+      // stroke(0)
+      // draw_lines(xpos, ypos)
       pop();
     }
   }
@@ -30,15 +28,30 @@ function setup() {
 function draw() {
 }
 
-function draw_curves() {
-  fill(0);
-  arc1 = arc(size/2, -size/2, size, size, PI * 0.5, PI)
-  arc2 = arc(-size/2, size/2, size, size, -PI * 0.5, 0)
-  return arc1, arc2
+function draw_curves(xpos, ypos) {
+  // translate(xpos + (size / 2), ypos + (size / 2))
+  // rotate(floor(random(4)) * PI * 0.5)
+  // translate(-xpos, -ypos)
+  // fill(0);
+  // strokeWeight(4)
+  arc1 = arc(xpos, ypos, size, size, PI * 0.5, PI)
+  arc2 = arc(xpos, ypos, size, size, -PI * 0.5, 0)
+  return arc1
 }
 
-function draw_lines() {
-  line1 = line(-size / 2, 0, size / 2, 0);
-  line2 = line(0, -size / 2, 0, size / 2);
+function draw_circs(xpos, ypos) {
+  translate(xpos + (size / 2), ypos + (size / 2))
+  rotate(floor(random(2)) * PI * 0.5)
+  translate(-xpos, -ypos)
+  // fill(0);
+  // strokeWeight(4)
+  arc1 = arc(xpos, ypos, size, size, PI * 0.5, PI)
+  arc2 = arc(xpos, ypos, size, size, -PI * 0.5, 0)
+  return arc1
+}
+
+function draw_lines(xpos, ypos) {
+  line1 = line(xpos + (size /2), ypos, xpos + (size /2), ypos + size);
+  line2 = line(xpos, ypos + (size /2), xpos + size, ypos + (size / 2));
   return line1, line2
 }
